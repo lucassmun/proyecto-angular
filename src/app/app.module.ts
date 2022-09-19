22765dce6aa15dba8febd32f64e6c2392b2a2121
lucassmun <lucassmun@gmail.com>
2022-09-18 22:47:16 -0300

    Primer desafío entregable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    ContenidoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
